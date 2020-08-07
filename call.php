c041ccc1089bd0c18484965e3984b8dd89cbf41c
cxx <cxx2320@foxmail.com>
2020-08-07 17:38:31 +0800

    call funciton

---
+++ call.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * @Author: cxx<cxx2320@foxmail.com>
+ * @Date: 2020-08-06 16:02:16
+ * @LastEditors: cxx
+ * @LastEditTime: 2020-08-06 16:04:54
+ */
+class A {
+    private $x = 1;
+}
+
+// PHP 7 之前版本的代码
+$getXCB = function () {
+    return $this->x;
+};
+
+$getX = $getXCB->bindTo(new A, 'A'); // 中间层闭包
+
+// // PHP 7+ 
+// $getX = function() {
+//     return $this->x;
+// };
+
+// echo $getx->call(new A);
