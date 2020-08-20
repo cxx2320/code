9672d02e96ede56ea17b847ce65e86fbf600043f
cxx <cxx2320@foxmail.com>
2020-08-20 16:44:18 +0800

    add swoole table add swoole process

---
+++ swoole/table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Swoole\Table;
+
+
+$table_config = [
+    'u2fd' => [
+        'size' => 10240,
+        'columns' => [
+            ['name' => 'fd', 'type' => Table::TYPE_INT, 'size' => 8]
+        ]
+    ],
+    'fd2u' => [
+        'size' => 10240,
+        'columns' => [
+            ['name' => 'uid', 'type' => Table::TYPE_INT, 'size' => 8]
+        ]
+    ],
+    'vote' => [
+        'size' => 256,
+        'columns' => [
+            ['name' => 'poll', 'type' => Table::TYPE_INT, 'size' => 8], //票数
+        ]
+    ],
+];
+
+$tables = [];
+
+foreach ($table_config as $table_name => $table) {
+    $table_obj = new Table($table['size']);
+    foreach ($table['columns'] as $column) {
+        $table_obj->column($column['name'], $column['type'], $column['size']);
+    }
+    $table_obj->create();
+    $tables[$table_name] = $table_obj;
+}
+var_dump($tables);
