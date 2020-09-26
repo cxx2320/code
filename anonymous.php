41c9048d6fd1bccf4040e8de910e24d5da96f17a
cxx <cxx2320@foxmail.com>
2020-09-26 11:22:59 +0800

    匿名类

---
+++ anonymous.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Q:如果匿名类被序列化和反序列化会怎么样？
+ * A:显然是不允许的。
+ */
+
+
+$obj = new class
+{
+    public $name = 'cxx';
+    public $age = 18;
+};
+
+var_dump(serialize($obj)); // PHP Fatal error:  Uncaught Exception: Serialization of 'class@anonymous' is not allowed
