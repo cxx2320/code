04f9ad9d5600a6e18c1fffd8756e08a12663f991
cxx <cxx2320@foxmail.com>
2020-10-29 09:41:48 +0800

    异常

---
+++ exception.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * 异常会从上至下的匹配第一个符合的catch块
+ */
+class FooException extends Exception
+{
+}
+
+class BarException extends Exception
+{
+}
+
+try {
+    throw new \BarException('Exception');
+} catch (\Exception $th) {
+    var_dump(4);
+} catch (\Throwable $th) {
+    var_dump(1);
+} catch (\FooException $th) {
+    var_dump(2);
+} catch (\BarException $th) {
+    var_dump(3);
+}
